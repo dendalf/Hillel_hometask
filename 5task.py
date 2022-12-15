05e52880b6f2a02309f32ea1b4abbcfd7e4399ee
Denis Agarkov <dionisiy.agarkov@gmail.com>
2022-12-15 14:19:38 +0200

    Added 2 files in repository

---
+++ 5task.py
@@ -0,0 +1,41 @@
+import os
+import sqlite3
+
+from typing import List, Set
+
+
+def execute_query(query_sql: str) -> List:
+    '''
+    Функция для выполнения запроса
+    :param query_sql: запрос
+    :return: результат выполнения запроса
+    '''
+    db_pass = os.path.join(os.getcwd(), 'chinook.db')
+    connection = sqlite3.connect(db_pass)
+    cur = connection.cursor()
+    result = cur.execute(query_sql).fetchall()
+    connection.close()
+    return result
+
+
+def unwrapper(records: List) -> None:
+    '''
+    Функция для вывода результата выполнения запроса
+    :param records: список ответа БД
+    '''
+    for record in records:
+        print(*record)
+
+
+def get_repeat_customers() -> List:
+    query_sql = '''
+            SELECT FirstName, COUNT(FirstName)
+            FROM customers
+            GROUP BY FirstName;
+        '''
+
+    return execute_query(query_sql)
+
+
+result = get_repeat_customers()
+unwrapper(result)
